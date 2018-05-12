7e04318db797c34488aeb56e10b6cac17ff0a423
Ralf Jung <post@ralfj.de>
2018-05-12 19:22:56 +0200

    handlebars template experiment

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,27 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+
+use std::collections::HashMap;
+
+use rocket_contrib::Template;
+
+#[get("/<foo>")]
+fn ff(foo: u32) -> String {
+    format!("Hello, world! {}", foo)
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", &())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        // TODO: Use Template::custom once rocket 0.4 is released, then we can e.g.
+        // call `handlebars.set_strict_mode`.
+        .attach(Template::fairing())
+        .mount("/", routes![ff, index])
+        .launch();
 }
