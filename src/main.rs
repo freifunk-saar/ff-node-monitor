b48134d0829e60a98c6b45c53f1865bfe6204fa3
Ralf Jung <post@ralfj.de>
2018-05-13 22:51:24 +0200

    add .env support

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,21 @@ extern crate rocket_contrib;
 extern crate r2d2;
 extern crate r2d2_diesel;
 extern crate diesel;
+extern crate dotenv;
 
 mod db;
 mod routes;
 
 // Launch the rocket
 fn main() {
+    // Load development environments
+    let _ = dotenv::dotenv();
+
     rocket::ignite()
         // TODO: Use Template::custom once rocket 0.4 is released, then we can e.g.
         // call `handlebars.set_strict_mode`.
         .manage(db::init_db_pool())
-        .attach(::rocket_contrib::Template::fairing())
+        .attach(rocket_contrib::Template::fairing())
         .mount("/", routes::routes())
         .launch();
 }
