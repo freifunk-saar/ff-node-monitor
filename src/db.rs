1f71ac95dd5b227d2dc4a81a8f587e5758d0e681
Ralf Jung <post@ralfj.de>
2018-05-13 22:40:26 +0200

    add managed DB connection

---
+++ src/db.rs
@@ -0,0 +1,49 @@
+use r2d2;
+use r2d2_diesel::ConnectionManager;
+use diesel::pg::PgConnection;
+
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+
+use std::{ops, env};
+
+/// Initializes a database pool.
+crate type ConnMgr = ConnectionManager<PgConnection>;
+crate type Pool = r2d2::Pool<ConnMgr>;
+
+crate fn init_db_pool() -> Pool {
+    let db_url = env::var("DATABASE_URL").expect("set DATABASE_URL to configure db connection");
+    let manager = ConnectionManager::<PgConnection>::new(db_url);
+    r2d2::Pool::builder()
+        .min_idle(Some(0))
+        .build(manager)
+        .expect("failed to create db pool")
+}
+
+// Connection request guard type: a wrapper around an r2d2 pooled connection.
+pub struct Conn(pub r2d2::PooledConnection<ConnMgr>);
+
+/// Attempts to retrieve a single connection from the managed database pool. If
+/// no pool is currently managed, fails with an `InternalServerError` status. If
+/// no connections are available, fails with a `ServiceUnavailable` status.
+impl<'a, 'r> FromRequest<'a, 'r> for Conn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Conn, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(Conn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+// For the convenience of using an &DbConn as an &PgConnection.
+impl ops::Deref for Conn {
+    type Target = PgConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-#![feature(plugin)]
+#![feature(plugin, crate_visibility_modifier)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
 extern crate rocket_contrib;
-
-use std::collections::HashMap;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate diesel;
 
 use rocket_contrib::Template;
+
+mod db;
 
 #[get("/<foo>")]
 fn ff(foo: u32) -> String {
@@ -22,6 +25,7 @@ fn main() {
     rocket::ignite()
         // TODO: Use Template::custom once rocket 0.4 is released, then we can e.g.
         // call `handlebars.set_strict_mode`.
+        .manage(db::init_db_pool())
         .attach(Template::fairing())
         .mount("/", routes![ff, index])
         .launch();
