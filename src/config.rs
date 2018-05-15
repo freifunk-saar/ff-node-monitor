83dd9300fb78d025f7176494ae1c162c28a60384
Ralf Jung <post@ralfj.de>
2018-05-15 16:29:39 +0200

    put postgres DB url into app config

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,18 @@
 use rocket;
+use rocket::fairing::{Fairing, AdHoc};
+
 use toml;
 use serde::Deserialize;
 use serde::de::IntoDeserializer;
+
+use db_conn;
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub instance_name: String,
     pub root_url: String,
     pub email_from: String,
+    pub postgres_url: String,
 }
 
 impl Config {
@@ -15,6 +20,19 @@ impl Config {
     pub fn new(table: &rocket::config::Table) -> Self {
         let val = toml::value::Value::Table(table.clone());
         Self::deserialize(val.into_deserializer())
-            .expect("[ff-node-monitor] config table has missing or extra value")
+            .expect("app config table has missing or extra value")
     }
+}
+
+pub fn fairing(section: &'static str) -> impl Fairing {
+    AdHoc::on_attach(move |rocket| {
+        let config = {
+            let config_table = rocket.config().get_table(section)
+                .expect(format!("[{}] table in Rocket.toml missing or not a table", section).as_str());
+            Config::new(config_table)
+        };
+        Ok(rocket
+            .manage(db_conn::init_db_pool(config.postgres_url.as_str()))
+            .manage(config))
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ extern crate r2d2;
 extern crate r2d2_diesel;
 #[macro_use] extern crate diesel;
 extern crate diesel_migrations;
-extern crate dotenv;
 extern crate ring;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
@@ -27,21 +26,9 @@ mod util;
 mod config;
 
 fn main() {
-    // Load development environments and env vars
-    let _ = dotenv::dotenv();
-    let db_url = std::env::var("DATABASE_URL").expect("set DATABASE_URL to configure db connection");
-
     // Launch the rocket
     rocket::ignite()
-        .manage(db_conn::init_db_pool(db_url.as_str()))
-        .attach(rocket::fairing::AdHoc::on_attach(|rocket| {
-            let config = {
-                let config_table = rocket.config().get_table("ff-node-monitor")
-                    .expect("[ff-node-monitor] table in Rocket.toml missing or not a table");
-                config::Config::new(config_table)
-            };
-            Ok(rocket.manage(config))
-        }))
+        .attach(config::fairing("ff-node-monitor"))
         // TODO: Use Template::custom once rocket 0.4 is released, then we can e.g.
         // call `handlebars.set_strict_mode`.
         .attach(rocket_contrib::Template::fairing())
