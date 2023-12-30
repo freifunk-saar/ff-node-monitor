967b7310d210f3aadaf9a5c9ad07c2c27e1a5379
Ralf Jung <post@ralfj.de>
2023-12-30 17:19:04 +0100

    migrate (back) to lettre for email sending

--- src/util.rs
+++ src/util.rs
@@ -14,20 +14,21 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::ops::Deref;
+use std::{ops::Deref, str::FromStr as _};
 
 use rocket::{
     form::{self, FromFormField},
     request::{self, FromRequest, Outcome},
-    Request, State, UriDisplayQuery,
+    Request, UriDisplayQuery,
 };
 use rocket_dyn_templates::Template;
 
-use anyhow::Result;
-use futures::compat::Future01CompatExt;
-use mail::{default_impl::simple_context, headers, smtp, Email, HeaderTryFrom, Mail};
+use anyhow::{bail, Result};
+use lettre::{
+    message::{header::ContentType, Mailbox},
+    Address, AsyncSmtpTransport, AsyncTransport as _, Message, Tokio1Executor,
+};
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
 
 use crate::config::Config;
 
@@ -82,39 +83,18 @@ impl Deref for EmailAddress {
     }
 }
 
-/// Horribly hacky hack to get access to the Request, and then a template's body, for building emails
+/// To send an email we need to render a template and for that we need to get access to the rocket,
+/// and that seems t require a `FromRequest` impl.
 pub struct EmailSender<'a> {
     rocket: &'a rocket::Rocket<rocket::Orbit>,
-    config: &'a Config,
-    mail_ctx: &'a simple_context::Context,
-}
-
-#[derive(Debug, Error)]
-enum EmailError {
-    #[error("{0}")]
-    ComponentCreation(mail::error::ComponentCreationError),
-    #[error("{0}")]
-    MailSend(mail::error::MailSendError),
 }
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for EmailSender<'r> {
     type Error = ();
     async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        let config = request
-            .guard::<&State<Config>>()
-            .await
-            .expect("config")
-            .inner();
-        let mail_ctx = request
-            .guard::<&State<simple_context::Context>>()
-            .await
-            .expect("mail_ctx")
-            .inner();
         Outcome::Success(EmailSender {
             rocket: request.rocket(),
-            config,
-            mail_ctx,
         })
     }
 }
@@ -127,41 +107,41 @@ impl<'r> EmailSender<'r> {
         vals: serde_json::Value,
         to: &str,
     ) -> Result<()> {
-        let email_text = Template::show(
-            self.rocket,
-            email_template,
-            self.config.template_vals(vals)?,
-        )
-        .unwrap();
+        let config = self.rocket.state::<Config>().unwrap();
+        let email_text =
+            Template::show(self.rocket, email_template, config.template_vals(vals)?).unwrap();
         let email_parts: Vec<&str> = email_text.splitn(3, '\n').collect();
         let (email_from, email_subject, email_body) =
             (email_parts[0], email_parts[1], email_parts[2]);
 
         // Build email
-        let from = <Email as HeaderTryFrom<_>>::try_from(self.config.ui.email_from.as_str())
-            .map_err(EmailError::ComponentCreation)?;
-        let mut mail = Mail::plain_text(email_body, self.mail_ctx);
-        mail.insert_headers(
-            headers! {
-                headers::_From: [(email_from, from)],
-                headers::_To: [to],
-                headers::Subject: email_subject
-            }
-            .map_err(EmailError::ComponentCreation)?,
-        );
+        let message = Message::builder()
+            .from(Mailbox::new(
+                Some(email_from.to_owned()),
+                config.ui.email_from.clone(),
+            ))
+            .to(Address::from_str(to)?.into())
+            .subject(email_subject)
+            .header(ContentType::TEXT_PLAIN)
+            .body(email_body.to_owned())
+            .unwrap();
 
         // Send email
-        let config = if self.config.secrets.get_smtp_host() == "localhost" {
-            smtp::ConnectionConfig::builder_local_unencrypted()
+        let smtp_host = config.secrets.get_smtp_host();
+        let mailer = if smtp_host == "localhost" {
+            AsyncSmtpTransport::unencrypted_localhost() // always port 25
+        } else {
+            AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(smtp_host)?
                 .port(25)
                 .build()
-        } else {
-            let smtp_host = self.config.secrets.get_smtp_host();
-            smtp::ConnectionConfig::builder_with_port(smtp_host.parse()?, 25)?.build()
         };
-        Ok(smtp::send(mail.into(), config, self.mail_ctx.clone())
-            .compat()
-            .await
-            .map_err(EmailError::MailSend)?)
+        let r = mailer.send(message).await?;
+        if !r.is_positive() {
+            bail!(
+                "sending email failed:\n{}",
+                r.first_line().unwrap_or("<no message>")
+            );
+        }
+        Ok(())
     }
 }
